1c63fc44a88c2d71363ed6db6b04f2353bb34f51
20106036 <20106036@mail.wit.ie>
2024-03-07 16:55:13 +0000

    >> Setter Tests and ConstructorAndGetter Tests now successful. >> All Tests now successful.

--- src/Product.java
+++ src/Product.java
@@ -5,13 +5,13 @@
  */
 public class Product {
 
-    private String productName ;    // max length = 20 characters, default value is "".
+    private String productName = "" ;    // max length = 20 characters, default value is "".
                                     // When constructor is called, if the name is >20 chars, you should
                                     // only store the first 20 characters (Hint: use substr())
 
-    private int productCode ;  // valid values 1000 - 5000 inclusive - default value is 5000
+    private int productCode = 5000;  // valid values 1000 - 5000 inclusive - default value is 5000
 
-    private double unitCost;  //valid values are any positive number - default to 1
+    private double unitCost = 1;  //valid values are any positive number - default to 1
 
     private boolean inCurrentProductLine;   // no validation required. Default
     /**
@@ -21,8 +21,13 @@ public class Product {
      * @param unitCost Unit cost of the product - valid values are any positive number
      */
     public Product(String productName, int productCode, double unitCost, boolean inCurrentProductLine) {
-       this.productName = productName;
-       this.productCode = productCode;
+       setProductCode(productCode);
+       if(productName != null) {
+           if (productName.length() <=20)
+               this.productName = productName;
+           else
+               this.productName = productName.substring(0,20);
+       }
        this.unitCost = unitCost;
        this.inCurrentProductLine = inCurrentProductLine;
     }
@@ -66,21 +71,29 @@ public class Product {
      * @param productCode The new Product Code
      */
     public void setProductCode(int productCode) {
+        if ((productCode >= 1000) && (productCode <= 5000)) {
             this.productCode = productCode;
+        }
     }
     /**
      * Updates the Product Name to the value passed as a parameter
      * @param productName The new Product Name
      */
     public void setProductName(String productName) {
-            this.productName = productName;
+        if(productName != null) {
+            if(productName.length() <= 20) {
+                this.productName = productName;
+            }
+        }
     }
     /**
      * Updates the Unit Cost to the value passed as a parameter
      * @param unitCost The new unit cost for the product
      */
     public void setUnitCost(double unitCost) {
+        if ((unitCost >= 1) && (unitCost <= 99999)) {
             this.unitCost = unitCost;
+        }
     }
     /**
      * Updates the boolean indicating whether the product is in the current product line or not.
@@ -109,10 +122,10 @@ public class Product {
 
              currentProduct = "N";
         }
-        return "Product description: " + productName + "\n" +
+        return "\nProduct description: " + productName + "\n" +
                 "product code: " + productCode + "\n"+
                 "unit cost: " + unitCost + "\n" +
-                "currently in product line: " + currentProduct;
+                "currently in product line: " + currentProduct + "\n";
     }
 
 }
--- test/ProductTest.java
+++ test/ProductTest.java
@@ -18,7 +18,7 @@ class ProductTest {
         //name 21 chars
         tv60Inches = new Product("Television 60 Inches", 5001, 245.99, false);
         //name 0 chars
-        noNameProduct = new Product("", 5000, 0, false);
+        noNameProduct = new Product(null, 5000, 0, false);
     }
 
     @AfterEach
@@ -51,12 +51,12 @@ class ProductTest {
             // Validation is: between 1000 and 5000 inclusive
 
             // Testing lower boundaries for product code, set at constructor level
-            assertEquals(999, tv42Inches.getProductCode());  // 999 is invalid - should default to 5000
+            assertEquals(5000, tv42Inches.getProductCode());  // 999 is invalid - should default to 5000
             assertEquals(1000, tv50Inches.getProductCode());  // 1000 is valid - value accepted
 
             //testing upper boundaries of product code, set at constructor level
             assertEquals(5000, noNameProduct.getProductCode()); // 5000 is valid - value accepted
-            assertEquals(5001, tv60Inches.getProductCode());    // 5001 is invalid - should default to 5000
+            assertEquals(5000, tv60Inches.getProductCode());    // 5001 is invalid - should default to 5000
         }
 
         @Test
